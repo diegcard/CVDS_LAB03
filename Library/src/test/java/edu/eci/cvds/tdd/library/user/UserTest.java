1ffeb1cbce94a95aa81680aeb1a3bb4eb04baf15
Diego Cardenas <diegosp778@gmail.com>
2024-08-31 12:44:33 -0500

    Generate tests for class Book and User

--- Library/src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
@@ -1,4 +1,24 @@
 package edu.eci.cvds.tdd.library.user;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
+
+    @Test
+    public void ShouldEqualsConstructorWithName(){
+        User user = new User();
+        user.setName("Diego");
+        assertEquals(user.getName(), "Diego");
+    }
+
+    @Test
+    public void ShouldEqualsId(){
+        User user = new User();
+        user.setId("312");
+        assertEquals(user.getId(), "312");
+    }
+
+
 }
